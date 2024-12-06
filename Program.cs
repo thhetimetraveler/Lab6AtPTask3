781b404a391077dd67058591b8144bd5f283c2a9
asafo <asafo@IAPC>
2024-12-06 09:54:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6AtPTask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int rows = 6;
+            const int cols = 3;
+
+            int[,] matrix = new int[rows, cols];
+
+            FillMatrix(matrix);
+
+            Console.WriteLine("Generated Matrix:");
+            PrintMatrix(matrix);
+
+            (int minValue, int minRow, int minCol) = FindMinElementByAbsoluteValue(matrix);
+            Console.WriteLine($"\nMinimum absolute value element: {minValue}");
+            Console.WriteLine($"Position: Row {minRow + 1}, Column {minCol + 1}");
+        }
+
+        static void FillMatrix(int[,] matrix)
+        {
+            Random random = new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(-100, 101);
+                }
+            }
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j],4} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static (int minValue, int row, int col) FindMinElementByAbsoluteValue(int[,] matrix)
+        {
+            int minValue = Math.Abs(matrix[0, 0]);
+            int minRow = 0, minCol = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (Math.Abs(matrix[i, j]) < minValue)
+                    {
+                        minValue = Math.Abs(matrix[i, j]);
+                        minRow = i;
+                        minCol = j;
+                    }
+                }
+            }
+
+            return (matrix[minRow, minCol], minRow, minCol);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lab6AtPTask3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab6AtPTask3")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("318ca57f-99ac-4a99-a0e5-b5bd538fad77")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
